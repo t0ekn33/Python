e30d772a32afa47259f554008cfd0e2a897186df
Salazar <salazar.tony@heb.com>
2020-01-20 16:50:41 -0600

    dict

--- py4e/dict_juliet.py
+++ py4e/dict_juliet.py
@@ -1,18 +1,28 @@
 """
 read the lines of the file, break each line into a list of words, 
-then loop though each of th ewords in the line an dcount each word using a dictionary.
+then loop though each of the words in the line and count each word using a dictionary.
 """
-
+import string
+pname = "py4e/" #prepend folder path
 fname = input("Filename:")
+fname = pname + fname
+
+print(fname)
 try:
     fhand = open(fname)
 except:
     print("File not found!", fname)
     exit()
 
-counts = dict()
+counts = dict() #create empty dictionary
 for line in fhand:
-    words = line.split()
+    line = line.rstrip()    #remove trailing spaces
+    line = line.translate(line.maketrans("", "", string.punctuation))   #Removes punctuation
+    line = line.lower()     #lowercase
+    words = line.split()    #splits on spaces (delimiter) and creates a list
+    print(words) 
+
+    #count the number of words in the list
     for word in words:
         if word not in counts:
             counts[word] = 1
